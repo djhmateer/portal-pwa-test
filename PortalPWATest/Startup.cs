0540a5346ce6f409832f0b0a8a9e73a782aef323
Dave Mateer <davemateer@gmail.com>
2020-12-08 18:44:53 +0000

    auto

--- PortalPWATest/Startup.cs
+++ PortalPWATest/Startup.cs
@@ -24,6 +24,7 @@ namespace PortalPWATest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddProgressiveWebApp();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
