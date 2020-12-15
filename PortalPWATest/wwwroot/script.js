eecf2c2514e76f3f909dd3a1a1ae35f6ea08a657
Dave Mateer <davemateer@gmail.com>
2020-12-15 14:17:16 +0000

    auto

--- PortalPWATest/wwwroot/script.js
+++ PortalPWATest/wwwroot/script.js
@@ -1,4 +1,7 @@
-﻿window.addEventListener('load', () => {
+﻿
+// https://web.dev/offline-fallback-page/#registering-the-service-worker
+// register the service worker once the app has loaded
+window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('service-worker.js');
     }
